d8a8a04464f31ab028d6c857588cfd0868a9e296
AginS <agins.main@gmail.com>
2019-12-15 21:56:47 +0300

    Готов интерфейс для создания "заявки"

--- HelpScreen.py
+++ HelpScreen.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'HelpScreen.ui'
+# Form implementation generated from reading ui file 'UI/HelpScreen.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.2
 #
@@ -19,11 +19,26 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.button = QtWidgets.QPushButton(self.centralwidget)
-        self.button.setGeometry(QtCore.QRect(150, 520, 113, 32))
+        self.button.setGeometry(QtCore.QRect(140, 540, 113, 32))
         self.button.setObjectName("button")
         self.name_in = QtWidgets.QPlainTextEdit(self.centralwidget)
         self.name_in.setGeometry(QtCore.QRect(20, 40, 361, 31))
         self.name_in.setObjectName("name_in")
+        self.adress_in = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.adress_in.setGeometry(QtCore.QRect(20, 110, 361, 31))
+        self.adress_in.setObjectName("adress_in")
+        self.symptom_in = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.symptom_in.setGeometry(QtCore.QRect(20, 180, 361, 351))
+        self.symptom_in.setObjectName("symptom_in")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 361, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 90, 361, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(20, 160, 361, 16))
+        self.label_3.setObjectName("label_3")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -34,5 +49,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Вызов помощи"))
         self.button.setText(_translate("MainWindow", "Отправить"))
+        self.label.setText(_translate("MainWindow", "Ваше Имя:"))
+        self.label_2.setText(_translate("MainWindow", "Ваш адрес:"))
+        self.label_3.setText(_translate("MainWindow", "Опишите, что с вами произошло:"))
--- main.py
+++ main.py
@@ -2,17 +2,33 @@ import sys
 from PyQt5 import QtWidgets
 
 # UI_generated
-
 import HelpScreen
+
 
 class FirstAidHelper(QtWidgets.QMainWindow, HelpScreen.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.button.clicked.connect(self.p)
+        self.button.clicked.connect(self.CreateRequest)
 
-    def p(self):
-        print("OKKK")
+    def CreateRequest(self):
+        name = self.name_in.toPlainText()
+        adress = self.adress_in.toPlainText()
+        symptom = self.symptom_in.toPlainText()
+
+        json_request = '''
+        {
+            "name": $name,
+            "adress": $adress,
+            "symptom": $symptom
+        }
+        '''
+
+        json_request = json_request.replace("$name", name)
+        json_request = json_request.replace("$adress", adress)
+        json_request = json_request.replace("$symptom", symptom)
+
+        print(json_request)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
