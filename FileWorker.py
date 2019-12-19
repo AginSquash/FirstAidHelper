d86723c59d4aa2a0a3d26b19d33f82b82f1e31fd
AginS <agins.main@gmail.com>
2019-12-20 02:57:22 +0300

    utf8 fix

--- FileWorker.py
+++ FileWorker.py
@@ -1,8 +1,9 @@
 
 def readFile():
     sympotms_dict = {}
-    f = open('FIRSTAIDHELPER.txt')
-    for line in f:
-        data = line.split("-", maxsplit=1)
-        sympotms_dict[data[0]] = data[1]
+    #f = open('FIRSTAIDHELPER.txt')
+    with open("FIRSTAIDHELPER.txt", encoding='utf-8') as file:
+        for line in file:
+            data = line.split("-", maxsplit=1)
+            sympotms_dict[data[0]] = data[1]
     return sympotms_dict
