61a54f884cc713b349cb807ed7f4771e467ca473
AginS <agins.main@gmail.com>
2019-12-20 03:00:33 +0300

    "ISO-8859-1" ?

--- FileWorker.py
+++ FileWorker.py
@@ -2,7 +2,7 @@
 def readFile():
     sympotms_dict = {}
     #f = open('FIRSTAIDHELPER.txt')
-    with open("FIRSTAIDHELPER.txt", encoding='utf-8') as file:
+    with open("FIRSTAIDHELPER.txt", encoding="ISO-8859-1") as file:
         for line in file:
             data = line.split("-", maxsplit=1)
             sympotms_dict[data[0]] = data[1]
